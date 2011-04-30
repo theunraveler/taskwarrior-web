2ff60e85d7b88b87afd364ff8000e49d46f4ddc7
Jake Bell <jake@theunraveler.com>
2011-04-29 19:27:06 -0500

    Subnav, etc.

--- taskwarrior.rb
+++ taskwarrior.rb
@@ -16,22 +16,36 @@ before do
   @current_page = request.path_info
 end
 
-# Root
+# Helpers
+helpers do
+  def format_date(timestamp)
+    format = Taskwarrior::Config.file.get_value('dateformat') || 'm/d/Y'
+    subbed = format.gsub(/([a-zA-Z])/, '%\1')
+    Time.at(timestamp.to_i).strftime(subbed)
+  end
+end
+
+# Redirects
 get '/' do
-  redirect '/pending'
+  redirect '/tasks/pending'
+end
+get '/tasks/?' do
+  redirect '/tasks/pending'
 end
 
 # Task routes
-get '/pending/?' do
+get '/tasks/pending/?' do
   @title = 'Pending Tasks'
+  @subnav = { '/tasks/pending' => 'Pending', '/tasks/completed' => 'Completed' }
   @tasks = Taskwarrior::Task.tasks
-  erb :index  
+  erb :listing  
 end
 
-get '/completed' do
+get '/tasks/completed/?' do
   @title = 'Completed Tasks'
-  @tasks = Taskwarrior::Task.tasks('completed')
-  erb :index  
+  @subnav = { '/tasks/pending' => 'Pending', '/tasks/completed' => 'Completed' }
+  @tasks = Taskwarrior::Task.tasks(:completed)
+  erb :listing
 end
 
 # Projects
