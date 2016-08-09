237272f4e2dfaf8aa93529bee57f909a5ff70b64
Matthew Di Ferrante <work@mattdf.com>
2016-08-10 00:19:14 +0100

    fix nested json issue

--- lib/taskwarrior-web/services/parser/json.rb
+++ lib/taskwarrior-web/services/parser/json.rb
@@ -3,7 +3,6 @@ require 'json'
 module TaskwarriorWeb::Parser::Json
   def self.parse(json)
     json.strip!
-    json = '[' + json + ']'
-    json == '[No matches.]' ? [] : ::JSON.parse(json.gsub(/\n/,','))
+    json == '[No matches.]' ? [] : ::JSON.parse(json)
   end
 end
